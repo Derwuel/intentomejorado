c324ec75585b7b59ff1edd46427f58ecfd5158bc
Marcos Hernandez <the.crazy.rabbit.000@gmail.com>
2023-11-27 20:57:12 -0300

    importacion de librerias

--- src/app/componentes/componentes.module.ts
+++ src/app/componentes/componentes.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './agregados/header/header.component';
+import { CustomInputComponent } from './agregados/custom-input/custom-input.component';
+import { LogoComponent } from './agregados/logo/logo.component';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    CustomInputComponent,
+    LogoComponent
+  ],
+  exports: [
+    HeaderComponent,
+    CustomInputComponent,
+    LogoComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class ComponentesModule { }
